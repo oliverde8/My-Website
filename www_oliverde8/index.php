c42cf49673482e4ea2c59c387a016f8f2275d494
oliverde8 <oliverde8@gmail.com>
2014-06-21 17:48:29 +0200

    Website designe changing to uikit

--- www_oliverde8/index.php
+++ www_oliverde8/index.php
@@ -4,8 +4,8 @@
 	define('OWEB_DIR_PAGES', "pages");
 	define('OWEB_DIR_FAPI', "fapi");
 
-	define('OWEB_HTML_DIR_CSS','http://files.oliver-decramer.com/web/sources/css');
-	define('OWEB_HTML_DIR_JS','http://files.oliver-decramer.com/web/sources/js');
+	define('OWEB_HTML_DIR_CSS','src/css');
+	define('OWEB_HTML_DIR_JS','src/js');
 	define('OWEB_DIR_DATA','../www_oliverde8_files/data');
 	define('OWEB_WWW_DATA','http://files.oliver-decramer.com/data');
 	define('OWEB_HTML_URL_IMG','http://files.oliver-decramer.com/web/img');
